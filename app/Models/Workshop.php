9a83a6da65133c94b0f33d0aefaadd1ebac484d9
Jitender Yadav1 <jitender.yadav1@GLC02G39Q8ML7H.local>
2022-05-31 09:20:40 +0530

    first commint with all test done

---
+++ app/Models/Workshop.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Date;
+
+class Workshop extends Model
+{
+    public function events()
+    {
+        return $this->belongsTo(Event::class,'event_id');
+    }
+
+}
